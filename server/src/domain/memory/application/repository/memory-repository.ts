e03758f9ae69ab73af5d34e2f7e51348f86d5bd0
alnmaurofranco <jefeson23@live.com>
2023-05-18 03:40:48 -0300

    ✨ feat(server): add contract interface memory repository

---
+++ server/src/domain/memory/application/repository/memory-repository.ts
@@ -0,0 +1,8 @@
+import { Memory } from '@domain/memory/enterprise/entity/memory'
+
+export interface MemoryRepository {
+  findManyRecent(): Promise<Memory[]>
+  findById(id: string): Promise<Memory | null>
+  create(memory: Memory): Promise<void>
+  delete(memory: Memory): Promise<void>
+}
