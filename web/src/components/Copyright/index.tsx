a2a56930452c579faf095cbbb9fec882f5603828
alnmaurofranco <jefeson23@live.com>
2023-05-19 03:52:35 -0300

    ✨ feat(web): add copyright component

---
+++ web/src/components/Copyright/index.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <footer className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="/"
+        className="text-gray-50 underline"
+      >
+        Rocketseat
+      </a>
+    </footer>
+  )
+}
